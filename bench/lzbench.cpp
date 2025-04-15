ebd53a2e6faf047da7a471f1eed1505fccf5a976
swsok <swsok@etri.re.kr>
2025-04-15 04:55:57 +0000

    under debugging..

--- bench/lzbench.cpp
+++ bench/lzbench.cpp
@@ -256,10 +256,11 @@ size_t common(uint8_t *p1, uint8_t *p2, size_t insize)
     while (*(p1++) == *(p2++)) {
         size++;
 	if (size >= insize) {
-		size = 0;
-		break;
+		return 0;
 	}
     }
+
+    printf("%d -  %c != %c\n", size, *--p1, *--p2);
 
     return size;
 }
